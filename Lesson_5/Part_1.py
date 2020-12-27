8709e53bf74e308c3b32ac27e04e4e051a6412b9
Daniil <m1lamanto@gmail.com>
2020-12-27 06:34:52 +0300

    [upd] пятая домашка

--- Lesson_5/Part_1.py
+++ Lesson_5/Part_1.py
@@ -1,8 +1,6 @@
 with open("text.txt", "w", encoding="utf-8") as text_list:
-    data = []
     while True:
         user_string = input()
-        if user_string:
+        if user_string == '':
             break
-        data.append(user_string)
-    text_list.writelines(user_string)
+        text_list.write(user_string+'\n')
